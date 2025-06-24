bb6ce7eeb385c723f7331993617233eb55439cea
Mavericks2019 <1015673037@qq.com>
2025-06-25 00:20:49 +0800

    seprate shaders

---
+++ testglcopy/build/CMakeFiles/objViewer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/objViewer_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/objViewer_autogen.dir/ParseCache.txt"
+  "objViewer_autogen"
+  "CMakeFiles/objViewer.dir/glwidget.cpp.o"
+  "CMakeFiles/objViewer.dir/main.cpp.o"
+  "CMakeFiles/objViewer.dir/objViewer_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/objViewer.dir/qrc_shaders.cpp.o"
+  "objViewer"
+  "objViewer.pdb"
+  "qrc_shaders.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/objViewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
